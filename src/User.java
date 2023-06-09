52ae1280d463e7b221c99354fba487966228acad
BoburFlexer <bobursakirov2008@gmail.com>
2023-06-09 17:51:06 +0600

    Initial Commit

---
+++ src/User.java
@@ -0,0 +1,22 @@
+public class User {
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        if (age<18){
+            this.age = 0;
+        } else if (age>100){
+            this.age = 0;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "User:" + "\n" +
+                "name:" + name + "\n" +
+                "age=" + age;
+    }
+}
